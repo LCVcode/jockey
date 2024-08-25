b8aaba4482568ea7411ccf83751611db36334c22
John Lettman <the@johnlettman.com>
2024-08-25 02:08:43 -0400

    Snap: use strict confinement (fix #48) (#50)
    * snapcraft.yaml: use strict confinement
    
    * __main__.py: isort

--- src/jockey/__main__.py
+++ src/jockey/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Jockey: a Juju query language to put all of your Juju objects at your fingertips."""
 from argparse import ArgumentParser, FileType, Namespace
+import os
 import sys
 from typing import Optional, Sequence
 
@@ -15,6 +16,10 @@ from jockey.core import (
     parse_filter_string,
 )
 from jockey.status_keeper import cache_juju_status, read_local_juju_status_file, retrieve_juju_cache
+
+
+if "SNAP" in os.environ:
+    os.environ["PATH"] += ":" + os.path.join(os.environ["SNAP"], "usr", "juju", "bin")
 
 
 INFO_MESSAGE = f"""
