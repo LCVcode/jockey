805b74eeb5a7a3cf9b2a9b840f2bce8e5114c9ae
John Lettman <the@johnlettman.com>
2024-09-01 15:30:11 -0400

    abstractions: add docs

--- src/jockey/abstractions.py
+++ src/jockey/abstractions.py
@@ -1,3 +1,66 @@
+"""
+This module defines a set of protocols and type abstractions for working with different Python types based on the
+compatibility of different comparison operations. The protocols are used to annotate types that support specific
+operations, such as ordering comparisons, equality comparisons, and containment checks.
+
+Protocols
+---------
+- `OrderingComparable`:
+    A protocol for annotating types that support ordering comparisons using the
+    `__lt__` (less than) and `__gt__` (greater than) methods.
+
+- `EqualityComparable`:
+    A protocol for annotating types that support equality comparisons using the `__eq__` (equals) method.
+
+- `NonEqualityComparable`:
+    A protocol for annotating types that support non-equality comparisons using the `__ne__` (not equals) method.
+
+- `OrderingEqualityComparable`:
+    A protocol for annotating types that support both ordering and equality comparisons.
+    It combines the functionality of `OrderingComparable` and `EqualityComparable`.
+
+- `ContainsComparable`:
+    A protocol for annotating types that support containment checks using the `__contains__` method.
+
+TypeVars
+--------
+- `O_C`:
+    A `TypeVar` bound to the `OrderingComparable` protocol,
+    representing any type that supports ordering comparisons.
+
+- `E_C`:
+    A `TypeVar` bound to the `EqualityComparable` protocol,
+    representing any type that supports equality comparisons.
+
+- `NE_C`:
+    A `TypeVar` bound to the `NonEqualityComparable` protocol,
+    representing any type that supports non-equality comparisons.
+
+- `OE_C`:
+    A `TypeVar` bound to the `OrderingEqualityComparable` protocol,
+    representing any type that supports both ordering and equality comparisons.
+
+- `C_C`:
+    A `TypeVar` bound to the `ContainsComparable` protocol,
+    representing any type that supports containment checks.
+
+- `C`:
+    A union type of `O_C`, `E_C`, `NE_C`, `OE_C`, and `C_C`,
+    representing any type that supports one or more of the defined comparability protocols.
+
+- `T`:
+    A generic `TypeVar` representing any type.
+
+Utility Methods
+---------------
+Each protocol includes a static method `is_valid` to check if a given object conforms to the protocol.
+
+Versions
+--------
+- 0.1.1: Initial version with protocol definitions and validation methods.
+"""
+
+
 from abc import ABC, abstractmethod
 from typing import Protocol, TypeVar, Union
 
@@ -16,13 +79,15 @@ class OrderingComparable(Protocol):
     @staticmethod
     def is_valid(obj: object) -> bool:
         """
-        Check if the given object is valid for the OrderingComparable protocol.
+        Check if the given object is valid for the `OrderingComparable` protocol.
 
-        Parameters:
-            obj (object): The object to be checked.
+        :param object obj:
+            The object to be checked.
 
-        Returns:
-            bool: Returns True if the object is a valid for the OrderingComparable protocol, otherwise False.
+        :returns: ``True`` if the object is a valid for the `OrderingComparable` protocol,
+            otherwise ``False``.
+
+        .. versionadded:: 0.1.1
         """
         return hasattr(obj, "__lt__") and hasattr(obj, "__gt__") and callable(obj.__lt__) and callable(obj.__gt__)
 
@@ -37,13 +102,15 @@ class EqualityComparable(Protocol):
     @staticmethod
     def is_valid(obj: object) -> bool:
         """
-        Check if the given object is valid for the EqualityComparable protocol.
+        Check if the given object is valid for the `EqualityComparable` protocol.
 
-        Parameters:
-            obj (object): The object to be checked.
+        :param object obj:
+            The object to be checked.
 
-        Returns:
-            bool: Returns True if the object is a valid for the EqualityComparable protocol, otherwise False.
+        :returns: ``True`` if the object is a valid for the `EqualityComparable` protocol,
+            otherwise ``False``.
+
+        .. versionadded:: 0.1.1
         """
         return hasattr(obj, "__eq__") and callable(obj.__eq__)
 
@@ -58,13 +125,15 @@ class NonEqualityComparable(Protocol):
     @staticmethod
     def is_valid(obj: object) -> bool:
         """
-        Check if the given object is valid for the NonEqualityComparable protocol.
+        Check if the given object is valid for the `NonEqualityComparable` protocol.
 
-        Parameters:
-            obj (object): The object to be checked.
+        :param object obj:
+            The object to be checked.
 
-        Returns:
-            bool: Returns True if the object is a valid for the NonEqualityComparable protocol, otherwise False.
+        :returns: ``True`` if the object is a valid for the `ContainsComparable` protocol,
+            otherwise ``False``.
+
+        .. versionadded:: 0.1.1
         """
         return hasattr(obj, "__ne__") and callable(obj.__ne__)
 
@@ -75,13 +144,15 @@ class OrderingEqualityComparable(OrderingComparable, EqualityComparable, ABC):
     @staticmethod
     def is_valid(obj: object) -> bool:
         """
-        Check if the given object is valid for the OrderingEqualityComparable protocol.
+        Check if the given object is valid for the `OrderingEqualityComparable` protocol.
 
-        Parameters:
-            obj (object): The object to be checked.
+        :param object obj:
+            The object to be checked.
 
-        Returns:
-            bool: Returns True if the object is a valid for the OrderingEqualityComparable protocol, otherwise False.
+        :returns: ``True`` if the object is a valid for the `OrderingEqualityComparable` protocol,
+            otherwise ``False``.
+
+        .. versionadded:: 0.1.1
         """
         return OrderingComparable.is_valid(obj) and EqualityComparable.is_valid(obj)
 
@@ -96,23 +167,36 @@ class ContainsComparable(Protocol):
     @staticmethod
     def is_valid(obj: object) -> bool:
         """
-        Check if the given object is valid for the ContainsComparable protocol.
+        Check if the given object is valid for the `ContainsComparable` protocol.
 
-        Parameters:
-            obj (object): The object to be checked.
+        :param object obj:
+            The object to be checked.
 
-        Returns:
-            bool: Returns True if the object is a valid for the ContainsComparable protocol, otherwise False.
+        :returns: ``True`` if the object is a valid for the `ContainsComparable` protocol,
+            otherwise ``False``.
+
+        .. versionadded:: 0.1.1
         """
         return hasattr(obj, "__contains__") and callable(obj.__contains__)
 
 
 O_C = TypeVar("O_C", bound=OrderingComparable)
+"""Ordering comparable type abstraction."""
+
 E_C = TypeVar("E_C", bound=EqualityComparable)
+"""Equality comparable type abstraction."""
+
 NE_C = TypeVar("NE_C", bound=NonEqualityComparable)
+"""Non-equality comparable type abstraction."""
+
 OE_C = TypeVar("OE_C", bound=OrderingEqualityComparable)
+"""Ordering and equality comparable type abstraction."""
+
 C_C = TypeVar("C_C", bound=ContainsComparable)
+"""Contains comparable type abstraction."""
 
 C = Union[O_C, E_C, NE_C, OE_C, C_C]
+"""Any comparable type abstraction."""
 
 T = TypeVar("T")
+"""Any type abstraction."""
